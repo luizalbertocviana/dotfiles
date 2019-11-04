da8b18c9343cec754142ac5c638c8ecfb5e151d6
Luiz Alberto <luizalbertocviana@gmail.com>
2019-11-04 17:19:33 -0300

    first version: fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+# SAGE
+# 8.0
+#set -gx PATH $PATH '/home/luiz/SAGE/SageMath'
+# 8.4
+#set -gx PATH '/home/luiz/SAGE/sage8.4/SageMath' $PATH 
+# 8.7
+set -gx PATH '/home/luiz/SAGE/sage8.7/SageMath' $PATH 
+
+# GUROBI
+set -gx PATH $PATH '/home/luiz/GUROBI/gurobi752/linux64/bin'
+set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH '/home/luiz/GUROBI/gurobi752/linux64/lib'
+set -gx GRB_LICENSE_FILE '/home/luiz/GUROBI/gurobi.lic'
+
+# Documentation
+set -gx MANPATH '/usr/share/man'
+
+# EDITOR
+set -gx EDITOR "vim"
+
+# GHC
+set -gx PATH '/home/luiz/.ghcup/bin' $PATH
