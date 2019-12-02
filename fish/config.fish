bde12e98f11093417214477d5b4ae94bcf6ac604
Luiz Alberto <luizalbertocviana@gmail.com>
2019-12-02 14:02:32 -0300

    commented some lines with no meaning in pargo3

--- fish/config.fish
+++ fish/config.fish
@@ -4,12 +4,12 @@
 # 8.4
 #set -gx PATH '/home/luiz/SAGE/sage8.4/SageMath' $PATH 
 # 8.7
-set -gx PATH '/home/luiz/SAGE/sage8.7/SageMath' $PATH 
+# set -gx PATH '/home/luiz/SAGE/sage8.7/SageMath' $PATH 
 
 # GUROBI
-set -gx PATH $PATH '/home/luiz/GUROBI/gurobi752/linux64/bin'
-set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH '/home/luiz/GUROBI/gurobi752/linux64/lib'
-set -gx GRB_LICENSE_FILE '/home/luiz/GUROBI/gurobi.lic'
+# set -gx PATH $PATH '/home/luiz/GUROBI/gurobi752/linux64/bin'
+# set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH '/home/luiz/GUROBI/gurobi752/linux64/lib'
+# set -gx GRB_LICENSE_FILE '/home/luiz/GUROBI/gurobi.lic'
 
 # Documentation
 set -gx MANPATH '/usr/share/man'
@@ -18,4 +18,4 @@ set -gx MANPATH '/usr/share/man'
 set -gx EDITOR "vim"
 
 # GHC
-set -gx PATH '/home/luiz/.ghcup/bin' $PATH
+# set -gx PATH '/home/luiz/.ghcup/bin' $PATH
