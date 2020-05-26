c570c23bda1908c0a476a7da467f9cfdd32e0d09
Luiz Alberto <luizalbertocviana@gmail.com>
2020-05-26 01:43:51 -0300

    straight package manager bootstrap

--- .emacs.el
+++ .emacs.el
@@ -73,6 +73,21 @@
 (setq user-full-name "Luiz Alberto do Carmo Viana")
 (setq user-mail-address "luizalbertocviana@gmail.com")
 
+;; straight package manager initialization
+(setq straight-use-package-by-default t)
+(setq straight-check-for-modifications nil)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;; bootstrap use-package
 (unless (package-installed-p 'use-package)
