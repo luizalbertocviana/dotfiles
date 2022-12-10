72d141ae68e2508723895003aaf9006aa825cfbf
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-12-10 01:01:56 -0300

    updates emacs config version

--- nix/nixpkgs/emacs.nix
+++ nix/nixpkgs/emacs.nix
@@ -11,8 +11,8 @@ homeConfig // {
         source = pkgs.fetchFromGitHub {
           owner = "luizalbertocviana";
           repo = "emacs.d";
-          rev = "5e9beefb1c1a0ecb2351792dfa97c130f831702d";
-          sha256 = "xnUv+16bSnwWeS/WnxS0oVQ6PsL8VIspGI/sGR2VV5w=";
+          rev = "bac671d34f1d7fc7799b27805de001498394e76f";
+          sha256 = "DrA46Cn3UpVyZcZtBltBr75lngmlxCSF396He57DK18=";
         };
         recursive = true;
       };
