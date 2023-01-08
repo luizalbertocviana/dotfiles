aa7563cd1fc68e35402ca019e1b3c393bf37b76c
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2023-01-08 19:56:41 -0300

    modifies scala setup

--- nix/nixpkgs/scala.nix
+++ nix/nixpkgs/scala.nix
@@ -3,7 +3,16 @@ pkgs: homeConfig:
 homeConfig // {
   home = homeConfig.home // {
     packages = homeConfig.home.packages ++ [
-      pkgs.sbt
+      pkgs.jdk
+      pkgs.coursier
     ];
+  };
+  programs = homeConfig.programs // {
+    bash = homeConfig.programs.bash // {
+      bashrcExtra = homeConfig.programs.bash.bashrcExtra + ''
+        export JAVA_HOME="${pkgs.jdk}/lib/openjdk"
+        export PATH="$PATH:$HOME/.local/share/coursier/bin"
+      '';
+    };
   };
 }
