0d3f7e03dd960a06c852e37c9876f1b586565720
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2024-11-06 15:23:52 -0300

    overrides fira code to be a nerd font

--- nix/nixpkgs/emacs.nix
+++ nix/nixpkgs/emacs.nix
@@ -3,7 +3,7 @@ pkgs: homeConfig:
 homeConfig // {
   home = homeConfig.home // {
     packages = homeConfig.home.packages ++ [
-      pkgs.fira-code # the font I use in emacs
+      (pkgs.nerdfonts.override { fonts =  [ "FiraCode" ];}) # the font I use in emacs
     ];
 
     file = (homeConfig.home.file or {}) // {
